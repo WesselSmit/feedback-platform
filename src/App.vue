902df578dc9153861469c12c413e0eed40a5df9f
Wessel Smit <wrsmit00@gmail.com>
2021-02-04 22:36:04 +0100

    access data on each component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <Navigation v-if="showNav" />
   <router-view />
+
+  <p>{{ this.user.name }}</p>
+  <Test />
 </template>
 
 <script>
 import Navigation from '@/components/Navigation.vue';
+import Test from '@/components/Test.vue';
 import { mapState } from 'vuex';
 
 export default {
   components: {
     Navigation,
+    Test,
   },
   computed: {
     ...mapState(['user']),
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>test component: {{ this.user.name }}</p>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['user']),
+  },
+};
+</script>
+
+<style>
+
+</style>
