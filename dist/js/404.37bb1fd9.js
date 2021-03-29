55eea584713f8f322bf92893ad8b06f42ff6fad6
Wessel Smit <wrsmit00@gmail.com>
2021-03-29 10:48:40 +0200

    build + added ignore rules to husky/lintstaged config

---
+++ dist/js/404.37bb1fd9.js
@@ -0,0 +1,6 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([['404'], {
+  '8cdb': function (n, e, c) {
+    c.r(e); const r = c('7a23'); function t(n, e, c, t, u, a) { return Object(r.p)(), Object(r.d)('h1', null, '404'); } const u = {}; u.render = t; e.default = u;
+  },
+}]);
+// # sourceMappingURL=404.37bb1fd9.js.map
---
+++ dist/js/settings.cc36c347.js
@@ -0,0 +1,9 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([['settings'], {
+  '26d3': function (e, t, n) {
+    n.r(t); const c = n('7a23'); function r(e, t, n, r, u, i) { return Object(c.p)(), Object(c.d)('p', null, 'settings'); } const u = {}; u.render = r; t.default = u;
+  },
+  3530(e, t, n) {
+    n.r(t); const c = n('7a23'); const r = Object(c.g)('p', null, 'admin', -1); const u = { key: 0 }; const i = Object(c.g)('p', null, 'you are a ADMIN!!', -1); function a(e, t, n, a, s, d) { return Object(c.p)(), Object(c.d)(c.a, null, [r, d.isAdmin ? (Object(c.p)(), Object(c.d)('div', u, [i])) : Object(c.e)('', !0)], 64); } const s = n('5530'); const d = n('5502'); const b = { computed: Object(s.a)(Object(s.a)({}, Object(d.c)('user', { userRole: 'role' })), {}, { isAdmin() { return this.userRole === 'admin'; } }) }; b.render = a; t.default = b;
+  },
+}]);
+// # sourceMappingURL=settings.cc36c347.js.map
