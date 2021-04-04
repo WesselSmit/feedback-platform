915406d9447792af75def88cfe1493eea265a02c
Wessel Smit <wrsmit00@gmail.com>
2021-04-04 11:47:20 +0200

    cleanup upload logic

--- src/pocs/Done.vue
+++ src/pocs/Done.vue
@@ -4,9 +4,10 @@
 
   <form ref="feedbackImageForm">
     <input type="file" ref="feedbackImageInput" @change="selectFile($event)">
-    <button @click.prevent="$refs.feedbackImageInput.click()">Browse</button>
-    <button @click.prevent="upload()">Upload</button>
+    <button @click.prevent="openFilePicker()">Browse</button>
   </form>
+
+  <button @click.prevent="upload()">Upload</button>
 </template>
 
 <script>
@@ -22,46 +23,43 @@ export default {
     };
   },
   methods: {
+    openFilePicker() {
+      this.$refs.feedbackImageInput.click();
+    },
     selectFile(e) {
       this.selectedFile = e.target.files[0];
     },
     async upload() {
-      const fileNameParts = this.selectedFile.name.split('.');
-      const extension = fileNameParts[fileNameParts.length - 1];
-      const allowedTypes = ['png', 'jpg', 'jpeg'];
-      const maxBytes = 1024 * 1024 * 5;
+      if (this.selectedFile) {
+        const fileNameParts = this.selectedFile.name.split('.');
+        const extension = fileNameParts[fileNameParts.length - 1];
+        const allowedTypes = ['png', 'jpg', 'jpeg'];
+        const maxBytes = 1024 * 1024 * 5;
 
-      if (allowedTypes.includes(extension) && this.selectedFile.size <= maxBytes) {
-        try {
-          const upload = storageRef.child(`feedback/${uuid()}`).put(this.selectedFile);
-          upload.on('state_changed',
-            (snapshot) => {
-              this.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              console.log(`Upload is ${this.progress}% done`);
-              switch (snapshot.state) {
-                case storage.TaskState.PAUSED:
-                  console.log('Upload is paused');
-                  break;
-                case storage.TaskState.RUNNING:
-                  console.log('Upload is running');
-                  break;
-                default:
-                  console.log('switch case not handled');
-              }
-            },
-            (err) => {
-              console.error('Error trying to upload file:', err);
-            },
-            async () => {
-              const downloadURL = await upload.snapshot.ref.getDownloadURL();
-              console.log('File available at', downloadURL);
-            });
-        } catch (err) {
-          console.error('Error trying to upload file:', err);
+        if (allowedTypes.includes(extension) && this.selectedFile.size <= maxBytes) {
+          try {
+            const upload = storageRef.child(`feedback/${uuid()}`).put(this.selectedFile);
+            upload.on('state_changed',
+              (snapshot) => {
+                this.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log(`Upload is ${this.progress}% done`);
+              },
+              (err) => {
+                console.error('Error trying to upload file:', err);
+              },
+              async () => {
+                const downloadURL = await upload.snapshot.ref.getDownloadURL();
+                console.log('File available at', downloadURL);
+              });
+          } catch (err) {
+            console.error('Error trying to upload file:', err);
+          }
+        } else {
+          this.$refs.feedbackImageForm.reset();
+          console.log('only .png and .jpg files smaller than 5mb allowed');
         }
       } else {
-        this.$refs.feedbackImageForm.reset();
-        console.log('only .png and .jpg files smaller than 5mb allowed');
+        console.log('no file selected');
       }
     },
   },
