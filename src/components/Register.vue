a7b8a586c83102f594a0bd0d8e926f08dc763971
Wessel Smit <wrsmit00@gmail.com>
2021-04-21 21:09:10 +0200

    forgot to add Register component to last commit

---
+++ src/components/Register.vue
@@ -0,0 +1,69 @@
+<template>
+  <form @submit.prevent>
+    <fieldset>
+      <h1>Sign up</h1>
+
+      <label for="signUpName">Name</label>
+      <input v-model.trim="signUpForm.name" type="text" placeholder="John Doe" id="signUpName" />
+
+      <label for="signUpGroup">I'm part of group</label>
+      <select v-model="signUpForm.group" id="signUpGroup">
+        <option v-for="group in groupNames" :key="group" :value="group">{{ group }}</option>
+      </select>
+
+      <label for="signUpEmail">Email</label>
+      <input v-model.trim="signUpForm.email" type="text" placeholder="you@email.com" id="signUpEmail" />
+
+      <label for="signUpPassword">Password</label>
+      <input v-model.trim="signUpForm.password" type="password" placeholder="min length is 6" id="signUpPassword" />
+
+      <button @click="handleSignup()" class="button">Sign Up</button>
+      <button @click="toggleForm()">Go to login</button>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'Register',
+  data() {
+    return {
+      signUpForm: {
+        name: '',
+        group: '',
+        email: '',
+        password: '',
+      },
+    };
+  },
+  computed: {
+    ...mapGetters('group', {
+      groupNames: 'groupNames',
+    }),
+  },
+  methods: {
+    ...mapActions('group', {
+      fetchGroups: 'fetchGroups',
+    }),
+    ...mapActions('user', {
+      signUp: 'signUp',
+    }),
+    handleSignup() {
+      // TODO voordat je de store code laat uitvoeren, check eerst of alle gegevens ingevuld zijn + handle errors (error messages) in UI
+      this.signUp(this.signUpForm);
+    },
+    toggleForm() {
+      this.$emit('handleToggleForm');
+    },
+  },
+  created() {
+    this.fetchGroups();
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
