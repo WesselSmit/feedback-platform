0d6a3bb6f64677ca4a8d02fbc659861646d9f1ca
Wessel Smit <wrsmit00@gmail.com>
2021-05-08 16:20:56 +0200

    improve Dashboard create project pop-up animation

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -208,10 +208,11 @@ export default {
     background-color: $white--overlay;
 
     &-inner {
-      @include zoomIn(300ms, $ease--fast);
+      @include zoomIn(500ms, 50ms, $ease--fast);
       width: $popup-width--small;
       padding: $space--sm-md;
       background-color: $white;
+      opacity: 0;
       border: $border--ui;
       border-radius: $border-radius;
     }
