d6fb85fc39e9a0513c0fc276e0718a79f7bdf0e1
Wessel Smit <wrsmit00@gmail.com>
2021-05-16 22:01:17 +0200

    added steps to Progressbar

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="progress-bar">
     <div class="progress-bar__container">
+      <div class="progress-bar__steps">
+        <div v-for="step in total - 1" :key="step" class="progress-bar__step"></div>
+      </div>
       <div class="progress-bar__value" :style="progress"></div>
     </div>
   </section>
@@ -13,7 +16,7 @@ export default {
   computed: {
     progress() {
       const progress = (this.index / this.total) * 100;
-      return { width: `${progress}%` };
+      return { width: `calc(${progress}% - 2.5px)` }; // 2.5px = half of $progressbar-gap-width
     },
   },
 };
@@ -29,10 +32,26 @@ export default {
     background-color: $gray--light;
   }
 
-  &__value{
+  &__value {
+    position: relative;
+    top: -#{$progressbar-height};
     height: $progressbar-height;
     background-color: $purple;
     transition: width 1s $ease--fast;
+  }
+
+  &__steps {
+    position: relative;
+    top: -#{$progressbar-height} * 2;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+  }
+
+  &__step {
+    width: $progressbar-gap-width;
+    height: $progressbar-height;
+    background-color: $white;
   }
 }
 </style>
